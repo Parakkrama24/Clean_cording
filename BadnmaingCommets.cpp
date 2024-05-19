89f3c8c55ee5cdee4bf4469d21d5cb0bec74828a
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-05-19 18:37:24 +0530

    a

--- BadnmaingCommets.cpp
+++ BadnmaingCommets.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+
+
+//This method calculates the employee's yearly salary 
+ double calculateAnnualSalary() { 
+    int monthlySalary=55000;
+ // Set i to monthlySalary
+ double i = monthlySalary; 
+ // Multiply i by 12 and assign to annualSalary
+ double annualSalary = i * 12; 
+ return annualSalary;
+}
+
 int main(){
 
     int diatance= 100;
